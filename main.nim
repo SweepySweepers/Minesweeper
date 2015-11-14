3c54c030451cc599bbf2bbac2db197ea2e691b43
Vlad <Dzyubavlad@gmail.com>
2015-11-14 16:12:09 +0200

    Initial commit

---
+++ main.nim
@@ -0,0 +1,11 @@
+import jester/jester, asyncdispatch, htmlgen
+
+routes:
+    get "/":
+        resp h1("Hello world")
+    get "/ss":
+        resp h2("Make me joke")
+    get "/request":
+        resp p(request.ip)
+
+runForever()
