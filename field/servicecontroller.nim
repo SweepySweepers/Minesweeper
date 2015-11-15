f6715ada30b31bbb4bee07639de170646f3e5812
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-11-15 16:24:30 +0200

    Basic WS server added

---
+++ field/servicecontroller.nim
@@ -0,0 +1,74 @@
+# Imports
+import websockets, cookies, marshal, strutils
+#import ../ircclient/manager, ../ircclient/log
+#import utilities, settings
+
+from times import getTime
+#from logincontroller import is_authenticated
+
+
+# Types
+type TClientMessage = object
+    target: string
+    server: string
+    message: string
+
+
+# Fields
+var service*: WebSocketServer
+
+
+# Procedures
+proc onBeforeConnect(ws: WebSocketServer, client: WebSocket, headers: StringTableRef): bool =
+    # Retrieve request's cookies
+    let c    = cookies.parseCookies(headers["Cookie"])
+    let auth = c["AUTH"]
+
+    # Validate user is logged in
+    #return auth != "" and
+    #       logincontroller.is_authenticated(auth)
+
+
+proc onMessage(ws: WebSocketServer, client: WebSocket, message: WebSocketMessage) =
+    # Save client message & send to IRC manager
+    var e       = to[TClientMessage](message.data)
+    var message = e.message
+
+    #if message.toLower.startsWith("/me "):
+    #    message = message.encodeAction()
+
+    #manager.sendMessage(message, e.server, e.target)
+
+    # Log message if it is not a command
+    #if message.find('/') != 0:
+    #    log.append TIRCEvent(
+    #        typ:        EvMsg,
+    #        cmd:        MPrivMsg,
+    #        nick:       settings.ircNick,
+    #        servername: e.server,
+    #        origin:     e.target,
+    #        params:     @[e.target, message],
+    #        timestamp:  getTime()
+    #    )
+
+
+proc onConnected(ws: WebSocketServer, client: WebSocket, message: WebSocketMessage) = discard
+
+
+proc onDisconnected(ws: WebSocketServer, client: WebSocket, message: WebSocketMessage) = discard
+
+
+proc initialize*(ws: var WebSocketServer) =
+    # Bind websocket server events
+    ws.onBeforeConnect = onBeforeConnect
+    ws.onConnected     = onConnected
+    ws.onMessage       = onMessage
+    ws.onDisconnected  = onDisconnected
+
+    service = ws
+
+    # Bind websocket to IRC log
+    #log.setIRCEventCallback(proc (e: string) =
+    #    for client in service.clients:
+    #        service.send(client, e)
+    #)
--- field/wsserver.nim
+++ field/wsserver.nim
@@ -1,39 +1,76 @@
 # Imports
-#import sockets, asyncio, jester, websockets
-import websockets
+import sockets, asyncio, jester, websockets, asyncdispatch
 #import settings, utilities, models
 
 # Import controllers
 #import logincontroller,
 #       homecontroller,
 #       servicecontroller
+import servicecontroller
 
 
 # Fields
-#var ws: TWebSocketServer
+var ws: WebSocketServer
 
 
 # Procedures
-#proc close*() {.noconv.} =
-#    ws.close()
+proc close*() {.noconv.} =
+    ws.close()
     #jester.close()
 
 
-#proc initialize*(dispatch: PDispatcher) {.closure, gcsafe.} =
+proc initialize*(dispatch: Dispatcher) =
 
     # Instantiate a websocket server
-    #ws = websockets.open(port = TPort(settings.serviceport), isAsync = true)
-    #ws = open(port = TPort(8080), isAsync = true) {.closure, gcsafe.}
+    #ws = websockets.open(port = Port(settings.serviceport), isAsync = true)
+    ws = open(port = sockets.Port(8080), isAsync = true)
 
     # Bind websocket server events
-    #servicecontroller.initialize(ws)
+    servicecontroller.initialize(ws)
 
     # Register web service with dispatcher
-    #dispatch.register(ws)
+    dispatch.register(ws)
 
     # Register jester with dispatcher
-    #jester.register(dispatch, port = TPort(settings.webPort), http = settings.useHTTP)
-    #jester.register(dispatch, port = TPort(5000), http = true)
+    #jester.register(dispatch, port = Port(settings.webPort), http = settings.useHTTP)
+    #jester.register(dispatch, port = Port(5000), http = true)
 
     # Register a quit procedure
-    #addQuitProc(close)
+    addQuitProc(close)
+
+
+let dispatch = asyncio.newDispatcher()
+
+
+# Procedures
+proc updateLoop() =
+    while dispatch.poll(): discard
+
+
+proc exit() {.noconv.} =
+    quit(QuitSuccess)
+
+#initialize dispatch
+
+
+
+
+######################################
+# Main entry point into iRC Familiar #
+######################################
+block main:
+
+    # Initialize log
+    #log.initialize()
+
+    # Initialize IRC
+    #manager.initialize(dispatch)
+
+    # Initialize Web Service
+    initialize(dispatch)
+
+    # Spint dispatcher
+    updateLoop()
+
+    # Set up CTRL+C sigint
+    setControlCHook(exit)
